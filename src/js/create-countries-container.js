d20488a1c10dc2cbefedd4d395ecd57dc4444319
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-24 23:23:25 +0200

    при полном вводе названия страны теперь она выводится (например Доминика не выводилась)

--- src/js/create-countries-container.js
+++ src/js/create-countries-container.js
@@ -5,6 +5,14 @@ import createErrorNotification from './notifications';
 
 export default function createCountriesContainer(countries) {
   if (countries.length > 1) {
+    countries.forEach(country => {
+      if (country.name.toLowerCase() === refs.input.value.toLowerCase()) {
+        const markup = oneCountryTemplate([country]);
+        refs.div.insertAdjacentHTML('beforeend', markup);
+        return;
+      }
+    });
+
     const markup = countriesTemplate(countries);
     refs.div.insertAdjacentHTML('beforeend', markup);
   } else if (countries.length === 1) {
